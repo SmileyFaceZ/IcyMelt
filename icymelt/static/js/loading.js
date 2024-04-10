26f59c6cdde070619ba215c114c3a6424d2c706e
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2024-04-10 17:06:01 +0700

    Implement loading template

---
+++ icymelt/static/js/loading.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    const loader = document.getElementById('loading-overlay');
+    loader.style.display = 'none';
+    const contents = document.getElementById('main-content');
+    contents.style.display = 'block';
+});
