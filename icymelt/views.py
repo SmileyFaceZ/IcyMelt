01f6260e8cf261a1ca1bc405e8328eb2556f9fd5
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2024-04-11 22:59:35 +0700

    Add forecatse box

--- icymelt/views.py
+++ icymelt/views.py
@@ -1,12 +1,19 @@
 from django.views.generic import TemplateView
 from icymelt.models import IceExp, Material, WeatherCondition
+from django.db.models import Avg
 
 
 class HomeView(TemplateView):
     template_name = "icymelt/home.html"
 
     def get_context_data(self, **kwargs):
-        return
+        context = super().get_context_data(**kwargs)
+        avg_temp = IceExp.objects.all().aggregate(avg_temp=Avg('temp'))['avg_temp']
+        context['avg_temp'] = round(avg_temp, 2)
+        avg_rh = IceExp.objects.all().aggregate(avg_rh=Avg('humidity'))['avg_rh']
+        context['avg_rh'] = round(avg_rh, 2)
+
+        return context
 
 
 class TableIceExpView(TemplateView):
