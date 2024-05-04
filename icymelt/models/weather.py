f09b18c6b33d1181dcfbfe2f61f7ff4621998918
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2024-05-04 10:50:37 +0700

    Add default model from weather API

--- icymelt/models/weather.py
+++ icymelt/models/weather.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class WeatherCondition(models.Model):
     """A model to represent weather conditions."""
-    weather = models.CharField(max_length=30)
+    code = models.IntegerField(null=True, unique=True)
+    weather = models.CharField(max_length=100)
 
     def __str__(self):
         return self.weather
